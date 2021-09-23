ba2272691ea04b094a434a1a005f1c93f8afaefc
Borys Olejniczak <borys.olejniczak@antarisconsulting.com>
2021-09-23 13:27:59 +0100

    initial commit

---
+++ apps/frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'blog-angular-nest-nx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'frontend';
+}
